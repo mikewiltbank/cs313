27b03782b2aa0fe7398385704509e6f59c07c0e7
Michael Wiltbank <michaelwiltbank@Michaels-MacBook-Air.local>
2017-01-14 14:48:35 -0700

    added homepage and connected files

---
+++ web/wiltbankhome.js
@@ -0,0 +1,20 @@
+
+// wiltbank02.js
+
+function clickAlert() {
+	alert("Clicked!");
+}
+//following function blocked out while using jquery
+/*
+function changeColor() {
+	var textbox_id = "txtColor";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
+}
+*/
