f07e516328e5872f25c20c7a0375f2037c25a25c
Michael Wiltbank <michaelwiltbank@Michaels-Air.netgear.com>
2017-01-21 22:53:03 -0700

    adding unfinished week03

--- web/week03/results.php
+++ web/week03/results.php
@@ -1,9 +1,34 @@
+<?php 
+	if(isset($_POST['submit'])){
+		$name = $_POST['boyname'];
+		$email = $_POST['director'];
+		$major = $_POST['major'];
+		$comments = $_POST['comments'];
+		$continents = $_POST['continent'];
+	}
 
+if ($name == "Orson") {
+	$_SESSION["orson"] = $_SESSION["orson"] + 1;
+}
+if ($name == "Spencer") {
+	$_SESSION["spencer"] = $_SESSION["spencer"] + 1;
+}
+if ($name == "Clyde") {
+	$_SESSION["clyde"] = $_SESSION["clyde"] + 1;
+
+}
+?>
+<!DOCTYPE html>
 <html>
+<head>
+	<title>Survey</title>
+</head>
 <body>
+		<label>I should name my newborn son:</label>
+		<p>Orson =<?php echo $_SESSION["orson"]; ?></p>
+</body>
+</html>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
