dd37844d96fe54fa8aa1211965a6e85e4feeb0db
Michael Wiltbank <michaelwiltbank@Michaels-Air.netgear.com>
2017-02-04 22:28:50 -0700

    user interface for project 1 updated but not finished

---
+++ web/week05/dbConnect.php
@@ -0,0 +1,37 @@
+<!--<?php
+try
+{
+  $user = 'wbsbfhpjjylesi';
+  $password = 'b698b588b5f9694c530f2918df16484b387affdc25c371646b0f4ab7304d6a75';
+  $db = new PDO('pgsql:host=ec2-54-243-197-180.compute-1.amazonaws.com;dbname=d1icv6q84sdjv7', $user, $password);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>-->
+
+<?php
+function get_db() {
+	$db = NULL;
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+        if (!isset($dbUrl) || empty($dbUrl)) {
+            $dbUrl = "postgres://ubuntu:cs313@127.0.0.1:8080/ovw";
+	    }
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	}
+	catch (PDOException $ex) {
+		die();
+	}
+	return $db;
+}
+?>
